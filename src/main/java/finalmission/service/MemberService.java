7ed7806a02e809778c0dde9f310cef4f76d6c533
DongchannN <ldc991104@gmail.com>
2025-06-16 00:13:24 +0900

    refactor: 외부 API 로직 분리

--- src/main/java/finalmission/service/MemberService.java
+++ src/main/java/finalmission/service/MemberService.java
@@ -4,9 +4,7 @@ import finalmission.domain.Gym;
 import finalmission.domain.Member;
 import finalmission.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
 @RequiredArgsConstructor
 @Service
@@ -16,9 +14,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final GymService gymService;
-
-    @Value("${randommer.api-key}")
-    private String randommorApiKey;
+    private final RandommerClientService randommerClientService;
 
     public Long addMember(String name, String phoneNumber, String password, Long gymId) {
         final boolean phoneNumberExist = memberRepository.existsByPhoneNumber(phoneNumber);
@@ -26,16 +22,7 @@ public class MemberService {
             throw new IllegalArgumentException("이미 가입한 핸드폰 번호입니다.");
         }
         final Gym gym = gymService.getGymById(gymId);
-        RestClient restClient = RestClient.builder().build();
-        final String[] body = restClient.get()
-                .uri("https://randommer.io/api/Name?nameType={type}&quantity={quantity}", "fullname", 1)
-                .header(RAMDOMMER_HEADER_KEY, randommorApiKey)
-                .retrieve()
-                .body(String[].class);
-        if (body == null) {
-            throw new IllegalArgumentException("닉네임 생성 중 에러가 발생하였습니다.");
-        }
-        final String nickname = body[0];
+        final String nickname = randommerClientService.getRandomNickname();
         final Member signupMember = Member.createSignupMember(name, nickname, phoneNumber, password, gym);
         return memberRepository.save(signupMember).getId();
     }
